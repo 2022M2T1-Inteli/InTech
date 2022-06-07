8beaaba084208bb672e5d0a3fcbd2c894b83ede2
Gabriel Carneiro <gabriel.carneiro@sou.inteli.edu.br>
2022-06-07 17:15:55 -0300

    Sistema de match integrado com banco de dados

--- src/Backend/Routes/match.js
+++ src/Backend/Routes/match.js
@@ -3,43 +3,45 @@ const sqlite3 = require("sqlite3").verbose() // Trazendo o sqlite3 (oficial do s
 const sqlite = require("sqlite") // Trazendo o sqlite (criado pela comunidade) para ser utilizado
 
 
+async function matchDoMilenio() {
+    const db = await sqlite.open({ filename: `../database/banco_de_dados.db`, driver: sqlite3.Database })
 
-async function matchDoMilenio(){
+    // pegando as habilidades da candidata e todas as vagas
+    const dbCandidataSkills = await db.get('SELECT habilidades_candidata FROM candidatas');
+    const dbVagas = await db.all('SELECT * FROM vagas');
 
-    const db = await sqlite.open({filename:`./database/banco_de_dados.db`,driver:sqlite3.Database})
+    // Comentei pq como ainda não estava tendo conexão pra pegar o ID estava dando erro no req.header
+    // const dbCandidataSkills = await db.get(`SELECT habilidades_candidata FROM candidatas WHERE id_candidata == ?`,[req.header.id]).then((res)=>{
+    //     habCandidata += res
+    // })
 
-    let habCandidata
-    let Vagas
+    // const dbVagasSkills = await db.all(`SELECT * FROM vagas `).then((res)=>{
+    //     Vagas += res
+    // })
 
-    await db.get(`SELECT habilidades_candidata FROM candidatas WHERE id_candidata == ?`,[req.header.id]).then((res)=>{
-        habCandidata += res
-    })
+    let candidataSkills = (dbCandidataSkills.habilidades_candidata).split(', ') //transformando as habilidades da candidata em um array
+    let idVagasMatched = [] //variável que guardará o ID das vagas que deram match
+   
+    for (let x = 0; x < dbVagas.length; x++) { //mapeando vaga por vaga
+        let vagaSkills = (dbVagas[x].habilidades_vaga).split(', ') //transformando as skills da vaga mapeada em um array
+        let matchedSkills = [] // guardará quais skills deram match
 
-    await db.all(`SELECT habilidades_vaga FROM vagas `).then((res)=>{
-        Vagas += res
-    })
-
-    let a = habCandidata.habilidade_candidata.split(",")
-
-    let JsonVagasMatch = []
-    let c = []
-    
-    for(let i = 0; i<Vagas.length; i++){
-        let b = Vagas[i].habilidades_vaga.split(",")
-
-        if(a.indexOf(b)){
-            
+        for (let i = 0; i < vagaSkills.length; i++) { // mapeando skill por skill 
+            if (vagaSkills.indexOf(candidataSkills[i]) > -1) { // quando uma skill não da match, retorna -1 e quando da match, retorna um número >= 0
+                matchedSkills.push(candidataSkills[i]) // guarda o valor da skill que deu match 
+            } 
         }
 
+        let matchPercent = matchedSkills.length /vagaSkills.length // calcula a porcentagem do match
 
-
-        
-
-        
+        //caso o match seja maior igual a 50% ele guarda o id da vaga na variável idVagasMatched
+        if (matchPercent >= 0.5) {
+            idVagasMatched.push(dbVagas[x].id_vaga) 
+        }
 
     }
-    
 
+    console.log(idVagasMatched)
 }
 
 matchDoMilenio()
