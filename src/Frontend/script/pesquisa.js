bcd0514322a73bc0371daeeb2db8ce3d84ec15f6
1zabella <99206817+1zabella@users.noreply.github.com>
2022-06-20 23:48:57 -0300

    Sistema de pesquisa e imagem da página de login atualizada

--- src/Frontend/script/pesquisa.js
+++ src/Frontend/script/pesquisa.js
@@ -1,14 +1,22 @@
-function search_animal() {
+//
+function pesquisa() {
     let input = document.getElementById('search').value
-    input=input.toLowerCase();
+    input = input.toLowerCase();
     let x = document.getElementsByClassName('vagas');
-      
-    for (i = 0; i < x.length; i++) { 
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item";                 
-        }
+    if (input.lenght >= 3) {
+        $.ajax({
+            url: 'http://localhost:3000/vaga/listAllVgasUser',
+            type: 'GET',
+            data: { input: input },
+            success: data => {
+                var list = '';
+                data.forEach(element => {
+                    list += `<li class="d-flex">${element.name}<div class="w-100"><button class="pull-right" id="btn-excluir" onclick="metodos.delete(${element.id})"><i class="fa fa-times" aria-hidden="true"></i></button><button class="pull-right" id="btn-edit" onclick="adicionarLinguagem(${element.id}, '${element.name}')"><i class="fa fa-pencil" aria-hidden="true"></i></button></div></li>`;
+                });
+                $('#linguagens').html(list);
+            }
+        });
     }
+
+    //criar uma nova rota que receba o valor do input acima
 }
